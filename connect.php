a92ac2ef55c6f79e23c7d2449acd83ea093ca81e
Chasith-Randima <chasith19@gmail.com>
2022-09-20 17:11:24 +0530

    connected php with sql database

--- connect.php
+++ connect.php
@@ -8,6 +8,6 @@ $database = "webfinal";
 $con = new mysqli($servername,$dbusername,$password,$database);
 
 if($con == false){
-    die("Error:could not connect".mysqli_connect_error());
+    die("Error:could not  connect".mysqli_connect_error());
 }
 ?>
