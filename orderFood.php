2a2d84cec1f5f9d744001e08257ff59ec9e056a6
Chasith-Randima <chasith19@gmail.com>
2022-10-01 17:11:24 +0530

    food order process completed

--- orderFood.php
+++ orderFood.php
@@ -35,7 +35,7 @@ include './navbar.php';
 <?php 
 include './connect.php';
 // session_start();
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in , if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
