60072600e62b496f3bcb43d25672759583402d76
Chasith-Randima <chasith19@gmail.com>
2022-11-01 17:11:24 +0530

    profile page created

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <?php 
 include './connect.php';
 session_start();
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in, if not then  redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
