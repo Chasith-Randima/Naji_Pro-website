189a7b412bf2628616fecf698f8abb6daf486bcb
Chasith-Randima <chasith19@gmail.com>
2022-11-05 17:11:24 +0530

    javascript functions done

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ window.onscroll = () => {
 };
 
 // document.querySelector("#search-icon").onclick = () => {
-//   document.querySelector("#search-form").classList.toggle("active");
+//   document.querySelector("#search-form").classList.toggle("active") ;
 // };
 
 document.querySelector("#close").onclick = () => {
