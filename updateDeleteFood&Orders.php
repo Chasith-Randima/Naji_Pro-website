42a2ac3bb3b3fd95f1b7d5b9737a1daff0526ed8
Chasith-Randima <chasith19@gmail.com>
2022-11-14 17:11:24 +0530

    rooms update cancel done

--- updateDeleteFood&Orders.php
+++ updateDeleteFood&Orders.php
@@ -3,7 +3,7 @@ include './connect.php';
 session_start();
 
 $userid = $_GET["userid"];
-// Check if the user is logged in, if not then redirect him to login page
+// Check if the user is logged in , if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
